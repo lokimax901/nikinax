73543477f26a68ca84a6e22303a3e4d24d48f0f2
ryanair901 <159541175+ryanair901@users.noreply.github.com>
2025-03-24 18:45:02 +0300

    Prepare for Railway deployment

--- app.py
+++ app.py
@@ -238,4 +238,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_test_account()
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
