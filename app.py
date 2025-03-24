76cc3dbb987f80cce755e3ade78c3b02dff6ba04
ryanair901 <159541175+ryanair901@users.noreply.github.com>
2025-03-24 18:49:24 +0300

    Switch from Railway to Render configuration

--- app.py
+++ app.py
@@ -238,5 +238,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_test_account()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(debug=True) 
